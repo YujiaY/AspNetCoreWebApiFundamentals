a8287dc4ed50e289d567f5c2aaf67a63c603bd9f
Jack Yuan <jack.yujia.yuan@gmail.com>
2024-10-05 15:06:20 +1000

    FilesController to get and upload files.

---
+++ CityInfo_Dev/Controllers/FilesController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/files")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+
+        private readonly FileExtensionContentTypeProvider _fileExtensionContentTypeProvider;
+
+        public FilesController(
+            FileExtensionContentTypeProvider fileExtensionContentTypeProvider)
+        {
+            _fileExtensionContentTypeProvider = fileExtensionContentTypeProvider
+                ?? throw new System.ArgumentNullException(
+                    nameof(fileExtensionContentTypeProvider));
+        }
+
+        [HttpGet("{fileId}")]
+        public ActionResult GetFile(string fileId)
+        {
+            // look up the actual file, depending on the fileId...
+            // demo code
+            var pathToFile1 = "Testing Document-1.pdf";
+            var pathToFile2 = "Testing Document-2.pdf";
+            var paths = new[] { pathToFile1, pathToFile2 };
+
+            // check whether the file exists
+            var pathToFile = paths.FirstOrDefault(p => p.Contains(fileId));
+            if (!System.IO.File.Exists(pathToFile))
+            {
+                return NotFound();
+            }
+
+            if (!_fileExtensionContentTypeProvider.TryGetContentType(
+                pathToFile, out var contentType))
+            {
+                contentType = "application/octet-stream";
+            }
+
+            var bytes = System.IO.File.ReadAllBytes(pathToFile);
+            return File(bytes, contentType, Path.GetFileName(pathToFile));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateFile(IFormFile file)
+        {
+            // Validate the input. Put a limit on filesize to avoid large uploads attacks. 
+            // Only accept .pdf files (check content-type)
+            if (file.Length == 0 || file.Length > 20971520 || file.ContentType != "application/pdf")
+            { 
+                return BadRequest("No file or an invalid one has been inputted.");
+            }
+
+            // Create the file path.  Avoid using file.FileName, as an attacker can provide a
+            // malicious one, including full paths or relative paths.  
+            var path1 = Directory.GetCurrentDirectory();
+            var path2 = Directory.GetDirectoryRoot(Directory.GetCurrentDirectory());
+            var path3 = Path.GetFullPath(path1);
+            DirectoryInfo? path4 = Directory.GetParent(Directory.GetCurrentDirectory());
+            var path = Path.Combine(
+                Directory.GetCurrentDirectory(),  
+                $"uploaded_file_{Guid.NewGuid()}.pdf");
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok("Your file has been uploaded successfully.");
+        }
+    }
+}
